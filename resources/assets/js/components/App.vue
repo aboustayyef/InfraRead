a10cfefa4e8bc03bc432d849f9b023933ab4a3e7
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-12-06 19:09:15 +0000

    minor improvements in UX

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -44,8 +44,11 @@
                 </div>
             </div>
         <div class="container" v-show="page == 'post list'">
-            <div class="row" v-if="filtered_posts.length == 0">
+            <div class="row" v-if="filtered_posts.length == 0 && posts_loaded">
                 There are no unread posts... <a @click="unread_only = false">See All posts</a>
+            </div>
+            <div class="row" v-if="! posts_loaded">
+                loading... 
             </div>
             <div v-if="posts_loaded" class='row'>
                 <ul>
@@ -110,6 +113,7 @@
         methods: {
             fetchPostList() 
             {
+                this.posts_loaded = false;
                 axios.get(this.posts_source).then((res) => {
                     this.posts = res.data;
                     this.posts_loaded = true;
