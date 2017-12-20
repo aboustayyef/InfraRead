87f9a9d98ba9558ddb2e91cbec0d38b943f74367
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-12-20 21:28:12 +0000

    restored a less chatty service worker

---
+++ public/service-worker.js
@@ -0,0 +1,26 @@
+// 'install' -- happens only the first time, or when a new version of service-worker.js is detected
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('feedreader').then(function(cache) {
+            return cache.addAll([
+            '/css/app.css',
+            '/js/app.js',
+            '/manifest.json'
+            ]);
+        })
+    );
+});
+
+// // 'activate' -- happens after installation
+// self.addEventListener('activate', function(event){
+//     console.log('The service worker has been activated');
+// });
+
+// 'fetch'
+self.addEventListener('fetch', function(event) {
+        event.respondWith(
+        caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+        })
+    );
+});
