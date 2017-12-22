2939bfde70710bd83f41d13729f2579fd1318dfc
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-12-22 10:42:29 +0000

    simplified UrlAnalyzer

--- app/UrlAnalyzer.php
+++ app/UrlAnalyzer.php
@@ -1,5 +1,6 @@
 <?php 
 namespace App;
+use Embed\Embed;
 use Guzzle\Http\Client;
 use Illuminate\Support\MessageBag;
 use Symfony\Component\DomCrawler\Crawler ;
@@ -11,14 +12,14 @@ use \Illuminate\Support\Collection;
 */
 class UrlAnalyzer
 {
-    public $status, $errorMessages, $result; 
-    private $html, $guzzleClient, $guzzleResponse, $crawler;
+    public $status, $error_messages, $result; 
+    private $url, $html, $embed;
 
     function __construct($url)
     {
         $this->url = $url;
         $this->status = 'ok';
-        $this->errorMessages = [];
+        $this->error_messages = [];
         $this->result = [];
 
         // Check url is valid
@@ -28,65 +29,23 @@ class UrlAnalyzer
 
 
         // Check url is accessible
-        $guzzleClient = new Client();
         try {       
-            $guzzleResponse = $guzzleClient->get($this->url)->send();
-        } catch (Exception $e) {
-            return $this->abort('Could not get content of URL. Please try again');
+            $this->embed = Embed::create($this->url);
+        } catch (\Embed\Exceptions\InvalidUrlException $e) {
+            return $this->abort('Cannot Access URL');
         }
-
-        if ($guzzleResponse->getStatusCode() !== 200) {
-            return $this->abort('Could not get content of URL. Please try again');
-        }
-
-        // get title and subtitle
-        $this->html = (string) $guzzleResponse->getBody();
-        $this->crawler = new Crawler($this->html);
-
-        // if any of the below cannot be found, we get status = 'warning' and show the missing items;
-        
-        $this->getTitleAndDescription();
-        $this->getRssFeed();
-
-        // $this->getDescription();
-        // $this->getRss();
-
-
+        $this->result['title'] = $this->embed->title;
+        $this->result['description'] = $this->embed->description;
+        $this->result['author'] = $this->embed->authorName;
+        $this->result['rss'] = $this->embed->feeds[0];
+        $this->result['canonical_url'] = $this->embed->url;
     }
 
-    private function urlIsValid($url){
-        return true;
-        // return preg_match('/^(https?:\/\/)?([\da-z\.-]+\.[a-z\.]{2,6}|[\d\.]+)([\/:?=&#]{1}[\da-z\.-]+)*[\/\?]?$/i', $url);
-    }
-
-    private function abort($message){
+    private function abort($message)
+    {
         $this->status = 'error';
-        $this->errorMessages[] = $message;
+        $this->error_messages[] = $message;
         return;
-    }
-
-    private function getTitleAndDescription(){
-        $rawTitle = $this->crawler->filter('title')->first()->text();
-        
-        // some titles come with description, split them;
-        $partsOfTitle = preg_split('#\s*\||–\s*#',$rawTitle);
-
-        $this->result['title'] = trim($partsOfTitle[0]);
-        if (isset($partsOfTitle[1])) {
-            $this->result['description'] = trim($partsOfTitle[1]);
-        }else{
-            $this->status = 'warning';
-            $this->errorMessages[] = 'Could not extract description';
-        }
-    }
-
-    private function getRssFeed(){
-        try {
-            $this->result['feed'] = $this->crawler->filter('link[type="application/rss+xml"]')->first()->attr('href');
-        } catch (Exception $e) {
-            $this->status = 'warning';
-            $this->errorMessages[] = 'Could not automatically extract RSS Feed';
-        }
     }
 }
 ?>
