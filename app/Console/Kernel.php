97d64d66d8e526149e7e7a17c6233ce471275042
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2019-04-11 17:22:25 +0000

    fixed error with scheduled commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('app:update_posts')
-            ->cron('*/' . config('infraread.refresh_interval') . ' * * * * *');
+            ->cron('*/10 * * * *');
     }
 
     /**
