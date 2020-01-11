92195166d25b21303602525791112b67d80099f5
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2020-01-11 14:56:26 +0000

    updated to laravel 5.7

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/administration/admin.js', 'public/js').version();
-mix.js('resources/assets/js/app.js', 'public/js').version()
-mix.sass('resources/assets/sass/app.scss', 'public/css').version()
-mix.sass('resources/assets/sass/administration/admin.scss', 'public/css').version() ;
+mix.js('resources/js/administration/admin.js', 'public/js').version();
+mix.js('resources/js/app.js', 'public/js').version()
+mix.sass('resources/sass/app.scss', 'public/css').version()
+mix.sass('resources/sass/administration/admin.scss', 'public/css').version() ;
