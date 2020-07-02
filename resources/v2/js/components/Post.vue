4ddeea557c5f1b2d028ab07f3fce52ce9cbf64cf
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2020-07-02 21:35:52 +0000

    added columns to content in columns view:

--- resources/v2/js/components/Post.vue
+++ resources/v2/js/components/Post.vue
@@ -4,15 +4,15 @@
       <div class="content is-marginless">
           <a :href="post.url"><h1 class="has-text-grey-dark title is-4 has-text-weight-bold">{{ post.title }}</h1></a>
           <h2 class="has-text-primary subtitle is-5 is-uppercase has-text-weight-semibold">{{ post.source.name }}</h2>
-          <p class="is-6 has-text-grey-light" >
-            {{post.time_ago}}
-            <span v-if="post.author">
-              by {{post.author}}
-            </span>
-          </p>          
+          	<p class="is-6 has-text-grey-light" >
+          	  {{post.time_ago}}
+          	  <span v-if="post.author">
+          	    by {{post.author}}
+          	  </span>
+          	</p>   
       </div>
       <hr>
-      <div class="content">
+      <div :class="{'content':true, 'has-columns':sanitized_content.length > 1200}">
           <div v-html="sanitized_content"></div>
        </div> 
     </div>
@@ -48,5 +48,10 @@ export default {
   border: 5px dashed silver;
   color: silver;
   text-transform: uppercase;
+}
+.has-columns{
+  column-width: 22em;
+    column-count:2;
+    column-rule: 1px dotted #ddd
 }
 </style>
