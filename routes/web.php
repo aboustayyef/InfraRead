f4d0f75271d8d7c2512f30b9743b89df193b1ef4
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2021-01-16 12:44:43 +0000

    restored default to original Vue app because of performance of livewire on server

--- routes/web.php
+++ routes/web.php
@@ -48,22 +48,22 @@ Route::get('/', function () {
     return redirect('/app');
 });
 
-Route::get('/app', function () {
-    return redirect('/app/v2');
-});
-
 // Route::get('/app', function () {
-//     $posts_source = '/api/posts';
-//     $posts_description = 'All Posts';
-//     $page = 'post list';
-//     $last_successful_crawl = getLastSuccesfulCrawl();
+//     return redirect('/app/v2');
+// });
 
-//     return view('home')
-//         ->with(compact('posts_source'))
-//         ->with(compact('page'))
-//         ->with(compact('posts_description'))
-//         ->with(compact('last_successful_crawl'));
-// })->middleware('auth');
+Route::get('/app', function () {
+    $posts_source = '/api/posts';
+    $posts_description = 'All Posts';
+    $page = 'post list';
+    $last_successful_crawl = getLastSuccesfulCrawl();
+
+    return view('home')
+        ->with(compact('posts_source'))
+        ->with(compact('page'))
+        ->with(compact('posts_description'))
+        ->with(compact('last_successful_crawl'));
+})->middleware('auth');
 
 Route::get('/app/source/{id}', function ($id) {
     $source = Source::findOrFail($id);
