bd2d28ca402156a09bacf7970c621acbaa9ff3b8
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2021-10-13 20:36:01 +0000

    removed bulma dependency from setup ui

--- resources/views/setup.blade.php
+++ resources/views/setup.blade.php
@@ -5,29 +5,29 @@
     <title>Setup</title>
     <!-- CSRF Stuff -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script> 
-    <link rel="stylesheet" type="text/css" href="{{mix('/css/app_bulma.css')}}">
+    <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
+    {{-- <link rel="stylesheet" type="text/css" href="{{mix('/css/app_bulma.css')}}"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <section class="hero is-medium is-primary is-bold">
-        <div class="hero-body">
-            <div class="container">
-                <h1 class="title">
+    <section class="container mx-auto mt-4">
+        <div class="">
+            <div class="">
+                <h1 class="text-3xl font-bold">
                     Hello :)
                 </h1>
-                <h2 class="subtitle">
+                <h2 class="mt-4 text-lg">
                     We noticed that you don't have any feeds yet
                 </h2>
                 <form action="/uploadOpml" method="post" enctype="multipart/form-data">
                     {{csrf_field()}}
-                    <div class="file">
+                    <div class="mt-4">
                         <label class="file-label">
                             <input class="file-input" type="file" name="opml">
                             <span class="file-cta">
                                 <span class="file-label">
-                                    Choose an OPML File 
+                                    Choose an OPML File
                                 </span>
                             </span>
                         </label>
@@ -35,7 +35,7 @@
                     <button style="display:none" id="nameOfFile" type="submit" class="button is-warning">Upload</button>
                     </div>
 
-                </form>                
+                </form>
                 <script>
                     // script to update name of selected file before uploading
                     let input_button = document.getElementsByClassName('file-input')[0];
