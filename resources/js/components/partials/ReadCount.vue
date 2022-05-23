88e232e5c0dbe0404f1813785580bf41fe47cb0d
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2022-05-23 17:24:22 +0000

    updated Inbox Zero Graphic

--- resources/js/components/partials/ReadCount.vue
+++ resources/js/components/partials/ReadCount.vue
@@ -1,7 +1,7 @@
 <template>
         <div id="ReadCount" class="text-gray-500 uppercase">
             <span v-if="count > 0"> unread: {{count}} </span>
-            <span v-else>Done! Enjoy your day</span>
+            <span v-else>All Posts Read!</span>
         </div>
 </template>
 <script>
