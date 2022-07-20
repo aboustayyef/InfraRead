27b6c917cab4ca2e88a21d50185419a91936d068
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2022-07-20 07:11:20 +0300

    updated and added apple touch icon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         {{-- Transfer Variables from PHP to Js --}}
-        <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script> 
+        <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
         <link rel="stylesheet" type="text/css" href="{{mix('/css/app.css')}}">
+        <link rel="apple-touch-icon" href="/img/apple-touch-icon.png">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="theme-color" content="#da2525">
     </head>
