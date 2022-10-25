be8fc04203d2e1286687dae5837654e325dd812b
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2022-10-25 12:30:22 +0000

    feeds opml can now how local RSS links

--- resources/views/opml.blade.php
+++ resources/views/opml.blade.php
@@ -2,17 +2,21 @@
 <?xml-stylesheet type="text/xsl" href="/assets/opml.xsl"?>
 <opml version="2.0">
 
-   <head>
-      <title>Infraread subscriptions</title>
-   </head>
+    <head>
+        <title>Infraread subscriptions</title>
+    </head>
 
-   <body>
-      @foreach ($categories as $category)
-      <outline title="{{$category->description}}">
-         @foreach ($category->sources as $source)
-         <outline title="{{$source->name}}" text="{{$source->description}}" xmlUrl="{{$source->fetcher_source}}" type="rss" htmlUrl="{{$source->url}}" />
-         @endforeach
-      </outline>
-      @endforeach
-   </body>
+    <body>
+        @foreach ($categories as $category)
+        <outline title="{{$category->description}}">
+            @foreach ($category->sources as $source)
+            @if(env('OPML') == 'original')
+            <outline title="{{$source->name}}" text="{{$source->description}}" xmlUrl="{{$source->fetcher_source}}" type="rss" htmlUrl="{{$source->url}}" />
+            @else
+            <outline title="{{$source->name}}" text="{{$source->description}}" xmlUrl="{{env('APP_URL')}}/rss/{{$source->id}}" type="rss" htmlUrl="{{$source->url}}" />
+            @endif
+            @endforeach
+        </outline>
+        @endforeach
+    </body>
 </opml>
