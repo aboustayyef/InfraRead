fc44d0fc7f10fb9b00c3ff1378cc6f6eb2cca55e
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2023-12-25 07:57:34 +0000

    Minor fixes for Muted admin

--- resources/views/admin/muted.blade.php
+++ resources/views/admin/muted.blade.php
@@ -3,7 +3,7 @@
    <div class="flex w-full">
       <h2 class="mr-12 text-4xl font-bold text-gray-600">Muted Phrases</h2>
     </div>
-    <p class="mt-4 text-gray-500">If these phrases are found in post's title, the post will be skipped</p>
+    <p class="mt-4 text-gray-500">If these phrases are found in a post's title, the post will be skipped</p>
    <hr class="my-12">
    <livewire:muted></livewire:muted>
 
