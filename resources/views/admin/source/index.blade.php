cfca7ab5a39071feaaadf4a8e3fb31ea2d86d270
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2024-02-08 15:24:11 +0000

    Added link to Download OPML in admin

--- resources/views/admin/source/index.blade.php
+++ resources/views/admin/source/index.blade.php
@@ -1,7 +1,6 @@
 @extends('admin.layout')
 
 @section('content')
-
 <livewire:source-list></livewire:source-list>
 
 @stop
--- resources/views/livewire/source-list.blade.php
+++ resources/views/livewire/source-list.blade.php
@@ -1,5 +1,9 @@
 <div>
-    <h2 class="mr-4 text-4xl font-bold text-gray-600 lg:mr-12">Sources</h2>
+    <div class="flex justify-between">
+        <h2 class="mr-4 text-4xl font-bold text-gray-600 lg:mr-12">Sources</h2>
+        <a href="/feeds.opml" class="inline-block rounded-md px-3 py-2 mb-4 bg-red-50 hover:bg-red-200">&darr; Download OPML</a>
+    </div>
+
     <div class="flex flex-wrap w-full max-w-2xl items-center mt-4 space-x-4">
         <input type="text" placeholder="Filter Sources" wire:model="searchString" class="ir_input">
         <a href="{{route('admin.source.create')}}" class="hover:text-white hover:bg-primary bg-white text-primary px-4 py-2 border border-primary rounded-md block">+ Add Source </a>
