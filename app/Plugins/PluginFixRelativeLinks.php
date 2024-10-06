02cca59765a74c6802e23c8976008ba982c91f1e
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2024-10-06 08:31:12 +0000

    remove srcset attributes from kottke posts

--- app/Plugins/PluginFixRelativeLinks.php
+++ app/Plugins/PluginFixRelativeLinks.php
@@ -48,51 +48,15 @@ class PluginFixRelativeLinks implements PluginInterface
         return $domain;
     }
     private function convertRelativeLinksToAbsolute($htmlContent, $domain) {
-        $dom = new \DOMDocument;
+        // Remove any srcset attributes
+        $htmlContent = preg_replace('/\s*srcset="[^"]*"/i', '', $htmlContent);
 
-        // Suppress errors due to malformed HTML and load the HTML content
-        // Wrap the content with a proper HTML structure
-        @$dom->loadHTML('<!DOCTYPE html><html><body>' . $htmlContent . '</body></html>', LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        // Replace <img src="/... with the absolute domain
+        $htmlContent = str_replace('<img src="/', '<img src="' . rtrim($domain, '/') . '/', $htmlContent);
 
-        // Create a new XPath object
-        $xpath = new \DOMXPath($dom);
-
-        // Find all <a> elements with href attribute
-        $links = $xpath->query("//a[@href]");
-        foreach ($links as $link) {
-            $href = $link->getAttribute('href');
-            if (parse_url($href, PHP_URL_SCHEME) === null) { // If the href is relative
-                $link->setAttribute('href', rtrim($domain, '/') . '/' . ltrim($href, '/'));
-            }
-        }
-
-        // Find all <img> elements with src and srcset attributes
-        $images = $xpath->query("//img[@src]");
-        foreach ($images as $img) {
-            $src = $img->getAttribute('src');
-            if (parse_url($src, PHP_URL_SCHEME) === null) { // If the src is relative
-                $img->setAttribute('src', rtrim($domain, '/') . '/' . ltrim($src, '/'));
-            }
-
-            // Handle srcset attribute
-            $srcset = $img->getAttribute('srcset');
-            if ($srcset) {
-                $srcsetParts = explode(',', $srcset);
-                $absoluteSrcsetParts = [];
-                foreach ($srcsetParts as $part) {
-                    $urlDescriptor = preg_split('/\s+/', trim($part));
-                    $url = $urlDescriptor[0];
-                    if (parse_url($url, PHP_URL_SCHEME) === null) { // If the url is relative
-                        $url = rtrim($domain, '/') . '/' . ltrim($url, '/');
-                    }
-                    $absoluteSrcsetParts[] = $url . (isset($urlDescriptor[1]) ? ' ' . $urlDescriptor[1] : '');
-                }
-                $img->setAttribute('srcset', implode(', ', $absoluteSrcsetParts));
-            }
-        }
-
-        // Return the modified HTML content (extract the body content)
-        $bodyContent = $dom->saveHTML($dom->getElementsByTagName('body')->item(0));
-        return substr($bodyContent, 6, -7); // Remove the enclosing <body> tags
+        return $htmlContent;
     }
+
+
+
 }
