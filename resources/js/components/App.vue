f35e64780fb2d2cfddff00fd0289fe4f401bfe93
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2025-08-30 09:48:42 +0000

    improvement to article loading skeleton

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -380,16 +380,26 @@ export default {
             this.displayed_post = {
                 ...p,
                 content: `<div class="w-full mt-2 md:mt-4">
-                        <div class="mt-8 md:mt-0 space-y-4">
-                            <div class="h-3 md:h-4 bg-gray-100 rounded animate-pulse w-5/6"></div>
-                            <div class="h-3 md:h-4 bg-gray-100 rounded animate-pulse w-4/6"></div>
-                            <div class="h-3 md:h-4 bg-gray-100 rounded animate-pulse w-3/4"></div>
-                            <div class="h-3 md:h-4 bg-gray-100 rounded animate-pulse w-2/3"></div>
-                            <div class="h-3 md:h-4 bg-gray-100 rounded animate-pulse w-3/4"></div>
-                            <div class="h-3 mt-2 md:h-4 bg-gray-100 rounded animate-pulse w-4/6"></div>
-                            <div class="h-3 md:h-4 bg-gray-100 rounded animate-pulse w-3/4"></div>
-                            <div class="h-3 md:h-4 bg-gray-100 rounded animate-pulse w-2/3"></div>
-                            <div class="h-3 md:h-4 bg-gray-100 rounded animate-pulse w-3/4"></div>
+                        <div class="mt-8 space-y-4">
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-full"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-5/6"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-3/4"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-2/3"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-1/4"></div>
+                        </div>
+                        <div class="mt-8 space-y-4">
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-5/6"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-full"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-2/3"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-5/6"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-1/4"></div>
+                        </div>
+                        <div class="mt-8 space-y-4">
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-full"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-5/6"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-3/4"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-2/3"></div>
+                            <div class="h-4 bg-gray-100 rounded animate-pulse w-1/4"></div>
                         </div>
                     </div>`
             };
