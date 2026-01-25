c876942eebf36397d36a22afb88bf733f56994fb
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2026-01-25 11:18:19 +0000

    Revert "added apple splash screen"
    This reverts commit 225622efcf7203065bea8ca5e75f296231881136.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,9 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="theme-color" content="#B90C11">
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-        <meta name="apple-mobile-web-app-title" content="InfraRead">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -17,21 +14,6 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="manifest" href="/manifest.webmanifest">
-        <link rel="apple-touch-icon" href="/img/apple-touch-icon.png">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-750x1334.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-828x1792.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-1170x2532.png" media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-1179x2556.png" media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-1242x2208.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-1242x2688.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-1284x2778.png" media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-1290x2796.png" media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-1536x2048.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-1668x2224.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-1668x2388.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
-        <link rel="apple-touch-startup-image" href="/img/ios-splash/ios-splash-2048x2732.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
--- tests/Feature/PwaAssetsTest.php
+++ tests/Feature/PwaAssetsTest.php
@@ -36,28 +36,4 @@ class PwaAssetsTest extends TestCase
         $this->assertFileExists($serviceWorkerPath);
         $this->assertStringContainsString('self.addEventListener', File::get($serviceWorkerPath));
     }
-
-    public function test_ios_startup_images_exist(): void
-    {
-        $sizes = [
-            '640x1136',
-            '750x1334',
-            '828x1792',
-            '1125x2436',
-            '1170x2532',
-            '1179x2556',
-            '1242x2208',
-            '1242x2688',
-            '1284x2778',
-            '1290x2796',
-            '1536x2048',
-            '1668x2224',
-            '1668x2388',
-            '2048x2732',
-        ];
-
-        foreach ($sizes as $size) {
-            $this->assertFileExists(public_path("img/ios-splash/ios-splash-{$size}.png"));
-        }
-    }
 }
